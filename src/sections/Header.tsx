3f9e84ba55b24d47a2d4b9193d26e967429012f4
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-01-22 03:06:12 +0800

    Enhance Header component by adding menu icon and improving layout structure

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,7 @@
 import ArrowRight from '@/assets/arrow-right.svg'
 import Logo from '@/assets/logosaas.png';
 import Image from 'next/image';
+import MenuIcon from '@/assets/menu.svg';
 
 export const Header = () => {
   return (
@@ -11,8 +12,11 @@ export const Header = () => {
             <ArrowRight className="h-4 w-4 inline-flex justify-center items-center"></ArrowRight>
           </div>
       </div>
-      <div>
-          <Image src={Logo} alt="Logo" height={40} width={40} />
+      <div className="py-5">
+        <div className="container">
+            <Image src={Logo} alt="Logo" height={40} width={40} />
+            <MenuIcon className="h-5 w-5"/>
+        </div>
       </div>
     </header>
   );
