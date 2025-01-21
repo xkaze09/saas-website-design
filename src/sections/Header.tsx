421a41af28d5bb3b77526f124906811334586265
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-01-22 02:55:58 +0800

    Update Header component to include logo and semantic HTML structure

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,12 +1,19 @@
 import ArrowRight from '@/assets/arrow-right.svg'
+import Logo from '@/assets/logosaas.png';
+import Image from 'next/image';
 
 export const Header = () => {
   return (
-    <div className="flex justify-center items-center py-3 bg-black text-white text-small">
-        <div className="inline-flex gap-1 items-center">
-          <p>Get started for free</p>
-          <ArrowRight className="h-4 w-4 inline-flex justify-center items-center"></ArrowRight>
-        </div>
-    </div>
+    <header>
+      <div className="flex justify-center items-center py-3 bg-black text-white text-small">
+          <div className="inline-flex gap-1 items-center">
+            <p>Get started for free</p>
+            <ArrowRight className="h-4 w-4 inline-flex justify-center items-center"></ArrowRight>
+          </div>
+      </div>
+      <div>
+          <Image src={Logo} alt="Logo" height={40} width={40} />
+      </div>
+    </header>
   );
 };
