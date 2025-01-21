efecefd0779198fb3127bab2bbb2f5ca0f432db1
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-01-22 02:37:34 +0800

    Enhance Header component with styled layout and arrow icon for call-to-action

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,12 @@
+import ArrowRight from '@/assets/arrow-right.svg'
+
 export const Header = () => {
-  return 'header';
+  return (
+    <div className="flex justify-center items-center py-3 bg-black text-white text-small">
+        <div className="inline-flex gap-1 items-center">
+          <p>Get started for free</p>
+          <ArrowRight className="h-4 w-4 inline-flex justify-center items-center"></ArrowRight>
+        </div>
+    </div>
+  );
 };
