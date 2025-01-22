ad6e6329356fd02617c01dcfb1618cc94a36f5a4
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-01-23 03:31:20 +0800

    Enhance Header component by adding a productivity message and adjusting text size and spacing for better visual appeal

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -6,7 +6,8 @@ import MenuIcon from '@/assets/menu.svg';
 export const Header = () => {
   return (
     <header className="sticky top-0">
-      <div className="flex justify-center items-center py-3 bg-black text-white text-small">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
+        <p className='text-white/60 hidden md:block'>Streamline your workflow and boost your productivity</p>
           <div className="inline-flex gap-1 items-center">
             <p>Get started for free</p>
             <ArrowRight className="h-4 w-4 inline-flex justify-center items-center"></ArrowRight>
