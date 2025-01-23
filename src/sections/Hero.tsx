e7af33a2113122f1c8c2e943ac38504e2ff74d47
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-01-24 00:14:41 +0800

    Implement Hero component with updated layout and messaging for version 2.0, enhancing user engagement and visual appeal

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,17 @@
 export const Hero = () => {
-  return "hero";
+  return (
+    <section>
+      <div className="container">
+        <div className="">
+          <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">Version 2.0 is here!</div>
+          <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text">Pathway to productivity</h1>
+          <p>Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your successes.</p>
+          <div className="">
+            <button>Get for free</button>
+            <button>Learn more</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 };
