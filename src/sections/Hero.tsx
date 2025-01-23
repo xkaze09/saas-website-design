01fa809e02b0a195c580153177b7269de53a7258
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-01-24 00:16:11 +0800

    Update Hero component with improved text styling and layout for version 2.0, enhancing user engagement and visual clarity

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,8 +5,8 @@ export const Hero = () => {
         <div className="">
           <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">Version 2.0 is here!</div>
           <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text">Pathway to productivity</h1>
-          <p>Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your successes.</p>
-          <div className="">
+          <p className="text-xl text-[#010D3E] tracking-tight mt-6">Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your successes.</p>
+          <div className="flex gap-1 items-center mt-[30px]">
             <button>Get for free</button>
             <button>Learn more</button>
           </div>
