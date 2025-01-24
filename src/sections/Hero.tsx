a196c1989e04174c74a5e4533ca817855354bd80
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-01-25 04:25:57 +0800

    Update Hero component layout to improve alignment and responsiveness, adding flexbox classes and adjusting image properties for better visual consistency.

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,7 +6,7 @@ export const Hero = () => {
   return (
     <section className="pt-8 pb-20 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_66%)]">
       <div className="container">
-        <div className="md:flex">
+        <div className="md:flex items-center">
         <div className="md:w-[478px]">
           <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">Version 2.0 is here!</div>
           <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text">Pathway to productivity</h1>
@@ -21,8 +21,8 @@ export const Hero = () => {
             </button>
           </div>
         </div>
-        <div className="mt-20 md:mt-0">
-          <Image src={cogImage} alt="Cog Image" />
+        <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
+          <Image src={cogImage} alt="Cog Image" className="md:absolutte md:h-full md:w-auto md:max-w-none"/>
         </div>
         </div>
       </div>
