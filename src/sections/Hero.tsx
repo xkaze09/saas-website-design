2a3986f8fdf33251c66f3801e34fe88f25181f2c
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-01-25 03:38:12 +0800

    Refactor Hero component layout to improve responsiveness and visual structure, adding flexbox for better alignment and adjusting margins for enhanced spacing.

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,7 +6,8 @@ export const Hero = () => {
   return (
     <section className="pt-8 pb-20 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_66%)]">
       <div className="container">
-        <div className="">
+        <div className="md:flex">
+        <div className="md:w-[478px]">
           <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">Version 2.0 is here!</div>
           <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text">Pathway to productivity</h1>
           <p className="text-xl text-[#010D3E] tracking-tight mt-6">Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your successes.</p>
@@ -20,8 +21,9 @@ export const Hero = () => {
             </button>
           </div>
         </div>
-        <div className="mt-20">
+        <div className="mt-20 md:mt-0">
           <Image src={cogImage} alt="Cog Image" />
+        </div>
         </div>
       </div>
     </section>
