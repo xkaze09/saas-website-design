8abf85e11fd5b1eb6fcd92a93becafdee8ee2ac7
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-01-30 03:44:39 +0800

    Add noodle image to Hero component assets

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,6 +1,7 @@
 import ArrowIcon from "@/assets/arrow-right.svg";
 import cogImage from "@/assets/cog.png";
 import cylinderImage from '@/assets/cylinder.png';
+import noodleImage from '@/assets/noodle.png';
 import Image from "next/image";
 
 export const Hero = () => {
