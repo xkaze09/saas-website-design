a0c66533697cf4e0daf8bfdd6599826e403a0775
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-01 15:50:20 +0800

    Enhance ProductShowcase section typography and layout

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -5,9 +5,11 @@ import Image from 'next/image';
 export const ProductShowcase = () => {
   return <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24">
     <div className="container">
-      <div className="tag">Boost your productivity</div>
-      <h2>A more effective way to track progress</h2>
-      <p>
+      <div className="flex justify-center">
+        <div className="tag">Boost your productivity</div>
+      </div>
+      <h2 className="text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">A more effective way to track progress</h2>
+      <p className="text-center">
             Effortlessly turn your ideas into a fully functional, responsive,
             SaaS website in just minutes.
       </p>
