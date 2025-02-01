ec7fd356c4ae75d098a03287101b9c2df0275ec4
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-01 22:45:55 +0800

    Add section-description CSS class to ProductShowcase section text

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -11,7 +11,7 @@ export const ProductShowcase = () => {
           <div className="tag">Boost your productivity</div>
         </div>
         <h2 className="section-title mt-5">A more effective way to track progress</h2>
-        <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
+        <p className="section-description mt-5">
               Effortlessly turn your ideas into a fully functional, responsive,
               SaaS website in just minutes.
         </p>
