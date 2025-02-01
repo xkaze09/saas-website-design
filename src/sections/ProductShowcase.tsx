be9e4f741e54e9c0f36ae70b4491eb6fdf0b7a0b
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-01 15:26:28 +0800

    Implement ProductShowcase section with content and product image

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,17 @@
+import productImage from '@/assets/product-image.png';
+import Image from 'next/image';
+
+
 export const ProductShowcase = () => {
-  return 'product showcase';
+  return <section>
+    <div className="container">
+      <div className="">Boost your productivity</div>
+      <h2>A more effective way to track progress</h2>
+      <p>
+            Effortlessly turn your ideas into a fully functional, responsive,
+            SaaS website in just minutes.
+      </p>
+      <Image src={productImage} alt="Product" className="w-full h-auto" />
+    </div>
+  </section>;
 };
