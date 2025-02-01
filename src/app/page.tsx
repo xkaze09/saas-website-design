09c44acd229cc2b35653566da485522ee68553df
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-01 18:20:36 +0800

    Add Pricing section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
+import { Pricing } from "@/sections/Pricing";
 import { ProductShowcase } from "@/sections/ProductShowcase";
 
 
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero />
       <LogoTicker />
       <ProductShowcase />
+      <Pricing />
     </>
     );
 }
