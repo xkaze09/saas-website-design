f962cd0bec3c79d4f938a0e4c2bec41a2cc4a112
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-01 16:28:17 +0800

    Improve ProductShowcase section layout and text alignment

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -5,14 +5,16 @@ import Image from 'next/image';
 export const ProductShowcase = () => {
   return <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24">
     <div className="container">
-      <div className="flex justify-center">
-        <div className="tag">Boost your productivity</div>
+      <div className="max-w-[540px] mx-auto">
+        <div className="flex justify-center">
+          <div className="tag">Boost your productivity</div>
+        </div>
+        <h2 className="text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">A more effective way to track progress</h2>
+        <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
+              Effortlessly turn your ideas into a fully functional, responsive,
+              SaaS website in just minutes.
+        </p>
       </div>
-      <h2 className="text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">A more effective way to track progress</h2>
-      <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
-            Effortlessly turn your ideas into a fully functional, responsive,
-            SaaS website in just minutes.
-      </p>
       <Image src={productImage} alt="Product" className="mt-10" />
     </div>
   </section>;
