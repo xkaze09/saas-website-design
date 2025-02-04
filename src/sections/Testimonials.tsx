71754a44f83adad64a4a8619d0fa31748fceb4c9
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-05 02:18:25 +0800

    Implement Testimonials card layout with user details and styling

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -77,9 +77,18 @@ export const Testimonials = () => {
       </div>
       <h2 className="section-title mt-5">What our users say</h2>
       <p className="section-description mt-5">From intuitive design to powerful features, our app has become an essential tool for users around the world.</p>
-      <div className="">
+      <div className="flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]">
         {firstColumn.map(({text, imageSrc, name, username}) => (
-          <div className="card"></div>
+          <div className="card">
+            <div>{text}</div>
+            <div className="flex items-center gap-2 mt-5">
+              <img src={imageSrc} alt={name} width={40} height={40} className="h-10 w-10 rounded-full" />
+              <div className="flex flex-col">
+                <div className="font-medium tracking-tight leading-5">{name}</div>
+                <div className="leading-5 tracking-tight">{username}</div>
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </div>
