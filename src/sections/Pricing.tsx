ca40b43a57bbfd06e701464fbd1191ad7b8a7ab7
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-04 23:22:36 +0800

    Refactor Pricing section card styling with Tailwind classes

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -65,7 +65,7 @@ export const Pricing = () => {
         </div>
         <div className="flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end lg:justify-center">
           {pricingTiers.map(({ title, monthlyPrice, buttonText, popular, inverse, features }, index) => (
-            <div key={index} className={twMerge("p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px-14px_#EAEAEA] max-w-xs w-full", inverse && 'border-black bg-black text-white')}>
+            <div key={index} className={twMerge("card", inverse === true && 'border-black bg-black text-white')}>
               <div className="flex justify-between">
                 <h3 className={twMerge("text-lg font-bold text-black/50", inverse && "text-white/60")}>{title}</h3>
                 {popular && (
