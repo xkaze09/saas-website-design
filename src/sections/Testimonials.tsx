aadf7f8181ed3e44939466eecf580f79aec20b44
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-04 22:12:32 +0800

    Add empty div with Testimonials text to section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -68,6 +68,7 @@ const testimonials = [
 export const Testimonials = () => {
   return <section>
     <div className="container">
+      <div className="">Testimonials</div>
       <h2>What our users say</h2>
       <p className="">From intuitive design to powerful features, our app has become an essential tool for users around the world.</p>
     </div>
