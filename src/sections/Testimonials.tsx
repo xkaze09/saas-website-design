67e12c88e122205d6ec39ac3b327b7a35542f968
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-10 12:10:52 +0800

    Remove third testimonials column and add className prop support

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -7,6 +7,7 @@ import avatar6 from "@/assets/avatar-6.png";
 import avatar7 from "@/assets/avatar-7.png";
 import avatar8 from "@/assets/avatar-8.png";
 import avatar9 from "@/assets/avatar-9.png";
+import { twMerge } from "tailwind-merge";
 
 const testimonials = [
   {
@@ -69,8 +70,13 @@ const firstColumn = testimonials.slice(0, 3);
 const secondColumn = testimonials.slice(3, 6);
 const thirdColumn = testimonials.slice(6, 9);
 
-const TestimonialsColumn = (props: {testimonials:typeof testimonials}) => (
-  <div className="flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]">
+const TestimonialsColumn = (props: {className?: string; testimonials:typeof testimonials}) => (
+  <div 
+    className={twMerge(
+      "flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]", 
+      props.className
+    )}
+  >
   {props.testimonials.map(({text, imageSrc, name, username}) => (
     <div className="card">
       <div>{text}</div>
@@ -97,7 +103,6 @@ export const Testimonials = () => {
       <div className="flex justify-center gap-6">
       <TestimonialsColumn testimonials={firstColumn} />
       <TestimonialsColumn testimonials={secondColumn} />
-      <TestimonialsColumn testimonials={thirdColumn} />
       </div>
     </div>
   </section>;
