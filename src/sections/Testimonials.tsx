80c3bf1fa15cf811ad73bc3b90cde5af343364fd
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-11 17:08:11 +0800

    Restructure Testimonials section with dedicated section-heading div

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -96,15 +96,17 @@ const TestimonialsColumn = (props: {className?: string; testimonials:typeof test
 export const Testimonials = () => {
   return <section className="bg-white">
     <div className="container">
-      <div className="flex justify-center">
-        <div className="tag">Testimonials</div>
-      </div>
-      <h2 className="section-title mt-5">What our users say</h2>
-      <p className="section-description mt-5">From intuitive design to powerful features, our app has become an essential tool for users around the world.</p>
-      <div className="flex justify-center gap-6">
-      <TestimonialsColumn testimonials={firstColumn} />
-      <TestimonialsColumn testimonials={secondColumn} className="hidden md:flex"/>
-      <TestimonialsColumn testimonials={thirdColumn} className="hidden lg:flex"/>
+      <div className="section-heading">
+        <div className="flex justify-center">
+          <div className="tag">Testimonials</div>
+        </div>
+        <h2 className="section-title mt-5">What our users say</h2>
+        <p className="section-description mt-5">From intuitive design to powerful features, our app has become an essential tool for users around the world.</p>
+        </div>
+        <div className="flex justify-center gap-6">
+        <TestimonialsColumn testimonials={firstColumn} />
+        <TestimonialsColumn testimonials={secondColumn} className="hidden md:flex"/>
+        <TestimonialsColumn testimonials={thirdColumn} className="hidden lg:flex"/>
       </div>
     </div>
   </section>;
