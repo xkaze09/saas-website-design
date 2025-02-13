b7a98754ee9831df461542f1764c883a070d1e41
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-14 04:18:02 +0800

    Apply props changes

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -77,28 +77,25 @@ const secondColumn = testimonials.slice(3, 6);
 const thirdColumn = testimonials.slice(6, 9);
 
 const TestimonialsColumn = (props: {className?: string; testimonials:typeof testimonials}) => (
-  <div 
-    className={twMerge(
-      "flex flex-col gap-6 mt-10", 
-      props.className
-    )}
-  >
-  {[...new Array(2)].fill(0).map((_, index) => (
-    <React.Fragment key={index}>
-        {props.testimonials.map(({text, imageSrc, name, username}, index) => (
-    <div className="card" key={index}>
-      <div>{text}</div>
-      <div className="flex items-center gap-2 mt-5">
-        <Image src={imageSrc} alt={name} width={40} height={40} className="h-10 w-10 rounded-full" />
-        <div className="flex flex-col">
-          <div className="font-medium tracking-tight leading-5">{name}</div>
-          <div className="leading-5 tracking-tight">{username}</div>
+  <div className={props.className}>
+    <div className="flex flex-col gap-6">
+    {[...new Array(2)].fill(0).map((_, index) => (
+      <React.Fragment key={index}>
+          {props.testimonials.map(({text, imageSrc, name, username}, index) => (
+      <div className="card" key={index}>
+        <div>{text}</div>
+        <div className="flex items-center gap-2 mt-5">
+          <Image src={imageSrc} alt={name} width={40} height={40} className="h-10 w-10 rounded-full" />
+          <div className="flex flex-col">
+            <div className="font-medium tracking-tight leading-5">{name}</div>
+            <div className="leading-5 tracking-tight">{username}</div>
+          </div>
         </div>
       </div>
-    </div>
-  ))}
-    </React.Fragment>
-  ))}
+    ))}
+      </React.Fragment>
+    ))}
+  </div>
 </div>
 )
 
@@ -112,10 +109,10 @@ export const Testimonials = () => {
         <h2 className="section-title mt-5">What our users say</h2>
         <p className="section-description mt-5">From intuitive design to powerful features, our app has become an essential tool for users around the world.</p>
         </div>
-        <div className="flex justify-center gap-6 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]">
+        <div className="flex justify-center gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]">
         <TestimonialsColumn testimonials={firstColumn} />
-        <TestimonialsColumn testimonials={secondColumn} className="hidden md:flex"/>
-        <TestimonialsColumn testimonials={thirdColumn} className="hidden lg:flex"/>
+        <TestimonialsColumn testimonials={secondColumn} className="hidden md:block"/>
+        <TestimonialsColumn testimonials={thirdColumn} className="hidden lg:block"/>
       </div>
     </div>
   </section>;
