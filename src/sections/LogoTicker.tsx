05fbe828f54d47a476749e52bfff730ecd1560f9
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-14 03:15:58 +0800

    Apply motion

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,4 @@
+"use client";
 import acmeLogo from '@/assets/logo-acme.png';
 import quantumLogo from '@/assets/logo-quantum.png';
 import echoLogo from '@/assets/logo-echo.png';
@@ -5,19 +6,21 @@ import celestialLogo from '@/assets/logo-celestial.png';
 import pulseLogo from '@/assets/logo-pulse.png';
 import apexLogo from '@/assets/logo-apex.png';
 import Image from 'next/image';
+import { motion } from "framer-motion";
+
 
 export const LogoTicker = () => {
   return <div className="py-8 md:py-12 bg-white">
       <div className="container">
         <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-          <div className="flex gap-14 flex-none">
+          <motion.div className="flex gap-14 flex-none">
             <Image src={acmeLogo} alt="Acme Logo" className="logo-ticker-image"/>
             <Image src={quantumLogo} alt="quantumLogo Logo" className="logo-ticker-image"/>
             <Image src={echoLogo} alt="echoLogo Logo" className="logo-ticker-image"/>
             <Image src={celestialLogo} alt="celestialLogo Logo" className="logo-ticker-image"/>
             <Image src={pulseLogo} alt="pulseLogo Logo" className="logo-ticker-image"/>
             <Image src={apexLogo} alt="apexLogo Logo" className="logo-ticker-image"/>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>;
