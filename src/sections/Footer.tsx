320e982a8abad23496ee66096328dd03a8fab9e5
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-13 23:13:29 +0800

    Implement detailed Footer component with logo, navigation, and social icons

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,33 @@
+import logo from '@/assets/logosaas.png';
+import Image from 'next/image';
+import SocialX from '@/assets/social-x.svg';
+import SocialInsta from '@/assets/social-insta.svg';
+import SocialLinkedIn from '@/assets/social-linkedin.svg';
+import SocialPin from '@/assets/social-pin.svg';
+import SocialYouTube from '@/assets/social-youtube.svg';
+
+
+
 export const Footer = () => {
-  return 'footer';
+  return <footer>
+    <div className="container">
+      <Image src={logo} alt="SaaS Logo" height={40} />
+      <nav>
+          <a href="#">About</a>
+          <a href="#">Features</a>
+          <a href="#">Customers</a>
+          <a href="#">Pricing</a>
+          <a href="#">Help</a>
+          <a href="#">Careers</a>
+      </nav>
+      <div className="social-icons">
+        <SocialX />
+        <SocialInsta />
+        <SocialLinkedIn />
+        <SocialPin />
+        <SocialYouTube />
+      </div>
+      <p>$copy; 2024 Your Company, Inc. All rights reserved.</p>
+    </div>
+  </footer>;
 };
