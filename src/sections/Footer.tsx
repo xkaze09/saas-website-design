a433613cd3d9631f204fcdd53ed9dede41c8ddbf
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-14 01:10:33 +0800

    Enhance Footer layout with flexbox and spacing

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -14,7 +14,7 @@ export const Footer = () => {
       <div className="inline-flex relative before:content-[''] before:h-full before:top-2 before:bottom-0 before:w-full before:blur before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8DFE)] before:absolute">
         <Image src={logo} alt="SaaS Logo" height={40} />
       </div>
-      <nav>
+      <nav className="flex flex-col gap-6 mt-6">
           <a href="#">About</a>
           <a href="#">Features</a>
           <a href="#">Customers</a>
@@ -22,14 +22,14 @@ export const Footer = () => {
           <a href="#">Help</a>
           <a href="#">Careers</a>
       </nav>
-      <div className="social-icons">
+      <div className="flex justify-center gap-6 mt-6">
         <SocialX />
         <SocialInsta />
         <SocialLinkedIn />
         <SocialPin />
         <SocialYouTube />
       </div>
-      <p>$copy; 2024 Your Company, Inc. All rights reserved.</p>
+      <p className='mt-6'>$copy; 2024 Your Company, Inc. All rights reserved.</p>
     </div>
   </footer>;
 };
