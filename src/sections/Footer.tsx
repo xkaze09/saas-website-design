f8a4a7cc6c68e1e0ac21cf5e07538401f0d2c189
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-14 01:09:03 +0800

    Add gradient blur effect to Footer logo container

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -11,7 +11,7 @@ import SocialYouTube from '@/assets/social-youtube.svg';
 export const Footer = () => {
   return <footer className='bg-black text-[#BCBCBC] text-sm py-10 text-center'>
     <div className="container">
-      <div className="inline-flex relative">
+      <div className="inline-flex relative before:content-[''] before:h-full before:top-2 before:bottom-0 before:w-full before:blur before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8DFE)] before:absolute">
         <Image src={logo} alt="SaaS Logo" height={40} />
       </div>
       <nav>
