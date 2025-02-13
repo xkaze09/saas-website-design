64075202d89206953e839928137982aa3cda330f
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-13 21:15:05 +0800

    Style CallToAction section with gradient background and responsive button layout

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,15 +1,15 @@
 export const CallToAction = () => {
   return (
-  <section>
+  <section className="bg-gradient-to-b from-white to-{#D2DCFF] py-24">
     <div className="container">
-      <h2>Sign up for free today</h2>
-      <p>
+      <h2 className="section-title">Sign up for free today</h2>
+      <p className="section-description mt-5">
         Celebrate the joy of accomplishments with an app designed to track your 
         progress and motivate your efforts.
       </p>
-      <div>
-        <button>Get for free</button>
-        <button>Learn more</button>
+      <div className="flex gap-2 mt-10 justify-center">
+        <button className="btn btn-primary">Get for free</button>
+        <button className="btn btn-text">Learn more</button>
       </div>
     </div>
   </section>
