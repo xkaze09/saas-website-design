80ccd002b68abfcc30fc2f6a35193b4ca86df079
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-13 21:30:01 +0800

    Add decorative star image to CallToAction section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,15 +1,21 @@
 import ArrowRight from '@/assets/arrow-right.svg';
+import starImage from '@/assets/star.png';
+import springImage from '@/assets/spring.png';
+import Image from 'next/image';
 
 
 export const CallToAction = () => {
   return (
   <section className="bg-gradient-to-b from-white to-{#D2DCFF] py-24">
     <div className="container">
-      <h2 className="section-title">Sign up for free today</h2>
-      <p className="section-description mt-5">
-        Celebrate the joy of accomplishments with an app designed to track your 
-        progress and motivate your efforts.
-      </p>
+      <div className="section-heading relative">
+        <h2 className="section-title">Sign up for free today</h2>
+        <p className="section-description mt-5">
+          Celebrate the joy of accomplishments with an app designed to track your 
+          progress and motivate your efforts.
+        </p>
+        <Image src={starImage} alt="Star Image" width={360} className=""/>
+      </div>
       <div className="flex gap-2 mt-10 justify-center">
         <button className="btn btn-primary">Get for free</button>
         <button className="btn btn-text gap-1">
