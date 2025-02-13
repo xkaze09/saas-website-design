5abe8e3b867f2a805c3e1257dcff2e283dc49db6
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-14 02:13:30 +0800

    Add responsive navigation layout to Footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -14,7 +14,7 @@ export const Footer = () => {
       <div className="inline-flex relative before:content-[''] before:h-full before:top-2 before:bottom-0 before:w-full before:blur before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8DFE)] before:absolute">
         <Image src={logo} alt="SaaS Logo" height={40} />
       </div>
-      <nav className="flex flex-col gap-6 mt-6">
+      <nav className="flex flex-col md:flex-row md:justify-center gap-6 mt-6">
           <a href="#">About</a>
           <a href="#">Features</a>
           <a href="#">Customers</a>
