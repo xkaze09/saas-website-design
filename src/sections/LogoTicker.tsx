5a177adb2e35a0763fed7109ce824d7fccaf6cf7
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-02-14 03:37:30 +0800

    Adjust animation for logos

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -13,11 +13,11 @@ export const LogoTicker = () => {
   return <div className="py-8 md:py-12 bg-white">
       <div className="container">
         <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-          <motion.div className="flex gap-14 flex-none" animate={{
+          <motion.div className="flex gap-14 flex-none pr-14" animate={{
             translateX: "-50%",
           }}
           transition={{
-            duration: 10,
+            duration: 20,
             repeat: Infinity,
             ease: "linear",
             repeatType: "loop",
